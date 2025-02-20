1a856ba2eef675b9ed620ee952fd546db222a329
sunkiteo-o <hyhy020301@gmail.com>
2025-02-20 20:31:49 +0900

    feat: 기본 정보 페이지 css

--- src/components/content/PersonalInfo.jsx
+++ src/components/content/PersonalInfo.jsx
@@ -11,15 +11,25 @@ const PersonalInfo = ({ info }) => {
             성함을 입력해주세요
             <span className="text-red-500"> *</span>
           </div>
-          <input />
+          <input className=" border border-[#E1E3ED] bg-[#E1E3ED] rounded-2xl text-[20px] py-[10px] px-[20px]" />
+
           <div className="text-[20px] text-[#E1E3ED] font-medium pt-[50px] pb-[30px]">
             이메일 주소를 입력해주세요
             <span className="text-red-500"> *</span>
           </div>
+          <input
+            className=" border border-[#E1E3ED] bg-[#E1E3ED] rounded-2xl text-[20px]  py-[10px] px-[20px]"
+            placeholder="예시: prography@gmail.com"
+          />
+
           <div className="text-[20px] text-[#E1E3ED] font-medium pt-[50px] pb-[30px]">
             휴대폰 번호를 입력해주세요
             <span className="text-red-500"> *</span>
           </div>
+          <input
+            className=" border border-[#E1E3ED] bg-[#E1E3ED] rounded-2xl text-[20px] py-[10px] px-[20px]"
+            placeholder="예시: 010-1234-5678"
+          />
         </div>
       </div>
     </div>
